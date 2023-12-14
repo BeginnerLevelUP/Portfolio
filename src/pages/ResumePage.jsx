762b5550005b678bf840e52fa135da9e62a336ab
Damion Morgan <only4school5680@gmail.com>
2023-12-14 18:24:29 -0500

    finished now time to add the styling

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,8 +1,31 @@
 function ResumePage() {
+    const skills =`
+    HTML
+    CSS
+    JS
+    BOOTSTRAP
+    JQUERY
+    EXPRESS
+    SQL 
+    MONGODB
+    SEQUELIZE
+    MONGOOSE
+    REACT
+    MERN STACK
+    VITE 
+    WEBPACK
+    etc..
+    `
     return (
+
         <>
             <h1>Resume</h1>    
-            <p>Content</p>  
+            <a href="">Link To Resume</a>
+            <h2>Developer Skills</h2>
+            <ul>
+                {/* Make into a list later */}
+                
+            </ul>
         </>
     )
 }
