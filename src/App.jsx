fd26b113b3bb22ce338e916cf27a8d5d99d807f5
Damion Morgan <only4school5680@gmail.com>
2024-01-01 18:12:39 -0500

    oopsie

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import { Outlet } from 'react-router-dom';
 import './App.css'
 //importing all my components
 import Header from './components/Header';
-
-
-import Footer from './components/footer';
+import Footer from './components/Footer';
 function App() {
 return(
     <>
