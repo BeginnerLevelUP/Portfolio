5644eb29e6b3ab2f8ccf241ef76b35affc88bb18
Damion Morgan <only4school5680@gmail.com>
2024-01-01 17:54:07 -0500

    added profile image

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -2,11 +2,12 @@ function AboutMePage() {
     return (
              <section class="about" id="about">
                 <aside>
-                    <h2>About remeber to add the image</h2>
+                    <h2>About</h2>
                 </aside>
     
                 <article>
                     <p> </p>
+                          <img src="../../public/profile.png"  />
                 </article>
 
             </section>
