5ba143f4f629a850939daa9323047e5a32b869c1
Damion Morgan <only4school5680@gmail.com>
2024-01-01 18:11:14 -0500

    files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
 import './App.css'
 //importing all my components
-import Header from './components/header';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
+
 
 import Footer from './components/footer';
 function App() {
